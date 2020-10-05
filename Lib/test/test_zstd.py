52f692f8cf7aafc64e2a9c60255520355f425a20
animalize <malincns@163.com>
2020-10-05 18:30:01 +0800

    ZstdFile: ValueError -> TypeError

--- Lib/test/test_zstd.py
+++ Lib/test/test_zstd.py
@@ -5,6 +5,7 @@ import pathlib
 import pickle
 import random
 import sys
+import tempfile
 from test import support
 import unittest
 
@@ -20,10 +21,11 @@ zstd = import_module("zstd")
 from zstd import ZstdCompressor, RichMemZstdCompressor, ZstdDecompressor, ZstdError, \
                  CParameter, DParameter, Strategy, compress, richmem_compress, decompress, \
                  ZstdDict, train_dict, finalize_dict, zstd_version, zstd_version_info, \
-                 compressionLevel_values, get_frame_info, get_frame_size
+                 compressionLevel_values, get_frame_info, get_frame_size, ZstdFile
 
 COMPRESSED_DAT = compress(b'abcdefg123456' * 1000)
 DAT_100_PLUS_32KB = compress(b'a' * (100 + 32*1024))
+DECOMPRESSED_DAT_100_PLUS_32KB = b'a' * (100 + 32*1024)
 SKIPPABLE_FRAME = (0x184D2A50).to_bytes(4, byteorder='little') + \
                   (100).to_bytes(4, byteorder='little') + \
                   b'a' * 100
@@ -523,6 +525,126 @@ class ZstdDictTestCase(unittest.TestCase):
             self.assertEqual(sample, dat2)
 
 
+class FileTestCase(unittest.TestCase):
+
+    def test_init(self):
+        with ZstdFile(BytesIO(DAT_100_PLUS_32KB)) as f:
+            pass
+        with ZstdFile(BytesIO(), "w") as f:
+            pass
+        with ZstdFile(BytesIO(), "x") as f:
+            pass
+        with ZstdFile(BytesIO(), "a") as f:
+            pass
+
+    def test_init_with_PathLike_filename(self):
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_f:
+            filename = pathlib.Path(tmp_f.name)
+
+        with ZstdFile(filename, "a") as f:
+            f.write(DECOMPRESSED_DAT_100_PLUS_32KB)
+        with ZstdFile(filename) as f:
+            self.assertEqual(f.read(), DECOMPRESSED_DAT_100_PLUS_32KB)
+
+        with ZstdFile(filename, "a") as f:
+            f.write(DECOMPRESSED_DAT_100_PLUS_32KB)
+        with ZstdFile(filename) as f:
+            self.assertEqual(f.read(), DECOMPRESSED_DAT_100_PLUS_32KB * 2)
+
+        os.remove(filename)
+
+    def test_init_with_filename(self):
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_f:
+            filename = pathlib.Path(tmp_f.name)
+            
+        with ZstdFile(filename) as f:
+            pass
+        with ZstdFile(filename, "w") as f:
+            pass
+        with ZstdFile(filename, "a") as f:
+            pass
+
+        os.remove(filename)
+
+    def test_init_mode(self):
+        bi = BytesIO()
+
+        with ZstdFile(bi, "r"):
+            pass
+        with ZstdFile(bi, "rb"):
+            pass
+        with ZstdFile(bi, "w"):
+            pass
+        with ZstdFile(bi, "wb"):
+            pass
+        with ZstdFile(bi, "a"):
+            pass
+        with ZstdFile(bi, "ab"):
+            pass
+
+    def test_init_with_x_mode(self):
+        with tempfile.NamedTemporaryFile() as tmp_f:
+            filename = pathlib.Path(tmp_f.name)
+            
+        for mode in ("x", "xb"):
+            with ZstdFile(filename, mode):
+                pass
+            with self.assertRaises(FileExistsError):
+                with ZstdFile(filename, mode):
+                    pass
+            os.remove(filename)
+
+    def test_init_bad_mode(self):
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), (3, "x"))
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "xt")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "x+")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "rx")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "wx")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "rt")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "r+")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "wt")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "w+")
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "rw")
+
+    def test_init_bad_check(self):
+        with self.assertRaises(TypeError):
+            ZstdFile(BytesIO(), "w", level_or_option='asd')
+        # CHECK_UNKNOWN and anything above CHECK_ID_MAX should be invalid.
+        with self.assertRaises(ZstdError):
+            ZstdFile(BytesIO(), "w", level_or_option={999:9999})
+        with self.assertRaises(ZstdError):
+            ZstdFile(BytesIO(), "w", level_or_option={CParameter.windowLog:99})
+
+        with self.assertRaises(TypeError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), "r", level_or_option=33)
+
+        with self.assertRaises(ValueError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB),
+                             level_or_option={DParameter.windowLogMax:2**31})
+
+        with self.assertRaises(ZstdError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), 
+                             level_or_option={444:333})
+
+        with self.assertRaises(TypeError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), zstd_dict={1:2})
+
+        with self.assertRaises(TypeError):
+            ZstdFile(BytesIO(DAT_100_PLUS_32KB), zstd_dict=b'dict123456')
+
+
 def test_main():
     run_unittest(
         FunctionsTestCase,
@@ -530,6 +652,7 @@ def test_main():
         CompressorDecompressorTestCase,
         DecompressorFlagsTestCase,
         ZstdDictTestCase,
+        FileTestCase,
     )
 
 if __name__ == "__main__":
